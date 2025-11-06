1c265b192c17a0780f222fac896a33334f171bd8
wynn <chenyinshu@gmail.com>
2025-11-06 18:59:05 +0800

    Initial Commit

---
+++ Hrv Run/Hrv_RunApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hrv_RunApp.swift
+//  Hrv Run
+//
+//  Created by 陈吟书 on 2025/11/6.
+//
+
+import SwiftUI
+
+@main
+struct Hrv_RunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hrv RunTests/Hrv_RunTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hrv_RunTests.swift
+//  Hrv RunTests
+//
+//  Created by 陈吟书 on 2025/11/6.
+//
+
+import Testing
+@testable import Hrv_Run
+
+struct Hrv_RunTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
